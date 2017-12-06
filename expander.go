16069e2feecdcd0d7b9a8dcefb453ba4852cfb43
kreamyx <karim.y.elghamrawy@gmail.com>
2017-12-06 11:20:31 -0800

    fixing paths for windows

--- expander.go
+++ expander.go
@@ -121,6 +121,7 @@ func ResolveRef(root interface{}, ref *Ref) (*Schema, error) {
 		json.Unmarshal(b, newSch)
 		return newSch, nil
 	default:
+		panic("yo yo")
 		return nil, fmt.Errorf("unknown type for the resolved reference")
 	}
 }
@@ -329,14 +330,24 @@ func normalizePaths(refPath, base string) string {
 	refURL, _ := url.Parse(refPath)
 	if path.IsAbs(refURL.Path) {
 		// refPath is actually absolute
-		return refPath
+		if refURL.Host != "" {
+			return refPath
+		}
+		return filepath.FromSlash(refPath)
 	}
 
 	// relative refPath
 	baseURL, _ := url.Parse(base)
 	if !strings.HasPrefix(refPath, "#") {
 		// combining paths
-		baseURL.Path = path.Join(path.Dir(baseURL.Path), refURL.Path)
+		if baseURL.Host != "" {
+			baseURL.Path = path.Join(path.Dir(baseURL.Path), refURL.Path)
+		} else { // base is a file
+			newBase := fmt.Sprintf("%s#%s", filepath.Join(filepath.Dir(base), filepath.FromSlash(refURL.Path)), refURL.Fragment)
+			log.Printf("Before Final Base: %+v", newBase)
+			return newBase
+		}
+
 	}
 	// copying fragment from ref to base
 	baseURL.Fragment = refURL.Fragment
@@ -440,11 +451,11 @@ func (r *schemaLoader) Resolve(ref *Ref, target interface{}, basePath string) er
 
 // absPath returns the absolute path of a file
 func absPath(fname string) (string, error) {
-	if path.IsAbs(fname) {
+	if filepath.IsAbs(fname) {
 		return fname, nil
 	}
 	wd, err := os.Getwd()
-	return path.Join(wd, fname), err
+	return filepath.Join(wd, fname), err
 }
 
 // ExpandSpec expands the references in a swagger spec
