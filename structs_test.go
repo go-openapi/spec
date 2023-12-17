94a1c0d6107d48df2acf85b961984d3f15d3212c
Frederic BIDON <fredbi@yahoo.com>
2023-12-17 18:51:16 +0100

    updated dependencies
    Signed-off-by: Frederic BIDON <fredbi@yahoo.com>

--- structs_test.go
+++ structs_test.go
@@ -20,7 +20,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 )
 
 func assertSerializeJSON(t testing.TB, actual interface{}, expected string) bool {
